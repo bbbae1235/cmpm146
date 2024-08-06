3bf82b434728c513d68fbd21081d0fed718ed8ab
Brian <bbbae@ucsc.edu>
2024-08-06 15:19:46 -0700

    trying to achieve 60% accuracy

--- p6/src/models/basic_model.py
+++ p6/src/models/basic_model.py
@@ -1,15 +1,34 @@
 from models.model import Model
-from tensorflow.keras import Sequential, layers
-from tensorflow.keras.layers.experimental.preprocessing import Rescaling
-from tensorflow.keras.optimizers import RMSprop, Adam
+from keras import Sequential, layers
+# from keras.layers.experimental.preprocessing import Rescaling
+from keras.optimizers import RMSprop, Adam
 
 class BasicModel(Model):
     def _define_model(self, input_shape, categories_count):
         # Your code goes here
         # you have to initialize self.model to a keras model
-        pass
-    
+        # before dense, flatten everything
+        self.model = Sequential()
+        # rescale to 0-1 range
+        self.model.add(layers.Rescaling(1./255, input_shape=input_shape))
+        # convolution layer
+        self.model.add(layers.Conv2D(16, (3, 3), activation='relu'))
+        self.model.add(layers.MaxPooling2D((3, 3)))
+        self.model.add(layers.Conv2D(48, (3, 3), activation='relu'))
+        self.model.add(layers.MaxPooling2D((3, 3)))
+        self.model.add(layers.Conv2D(72, (3, 3), activation='relu'))
+        self.model.add(layers.MaxPooling2D((3, 3)))
+        # flatten layer
+        self.model.add(layers.Flatten())
+        # dense layers
+        self.model.add(layers.Dense(96, activation='relu'))
+        self.model.add(layers.Dense(categories_count, activation='softmax'))
+       
     def _compile_model(self):
         # Your code goes here
         # you have to compile the keras model, similar to the example in the writeup
-        pass
+        self.model.compile(
+            optimizer=Adam(learning_rate=0.001),
+            loss='categorical_crossentropy',
+            metrics=['accuracy'],
+        )
